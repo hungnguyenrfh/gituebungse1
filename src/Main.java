2db9ad2c8df74c96708a976d592c4a1a974e765f
Luong Nguyen Hung Nguyen <177386014+hungnguyenrfh@users.noreply.github.com>
2025-12-15 12:50:15 +0100

    added: 7-mal "HelloUnderworld"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i < 7; i++){
-            System.out.println("Hello World");
+            System.out.println("Hello Underworld");
         }
     }
 }
